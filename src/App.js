1e11a4e531e58459c8ea9f736f3032f41e96456a
Wowaxur <wowaxur@yandex.ru>
2023-10-26 11:12:28 +0300

    Change app.js

--- src/App.js
+++ src/App.js
@@ -2,18 +2,49 @@ import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
-  return (
-
-    <div className="App">
-
-
-      <ul>
-        <li>Simple HTML</li>
-        <li>CSS</li>
-        <li>JS</li>
-      </ul>
-
-    </div>
-  );
+  return ( <div>
+          <Tech />
+          <Header />
+          <vldWow />
+      </div>
+);
 }
+const Header = () =>{
+    return(<div><center>
+            Hello, it VLD <a href="https://github.com/Wowaxur" target='_blank'>WOWAXUR</a>
+        </center>
+    </div>
+    );
+}
+
+const Tech = () => {
+   return( <div className="App">
+        <ul>
+            <li>Simple HTML</li>
+            <li>CSS</li>
+            <li>JS</li>
+        </ul>
+    </div>);
+}
+const vldWow = () => {
+    return(<div>
+            Hello, it VLD WOWAXUR
+        </div>
+    );
+ }
+
+
 export default App;
+// function Welcome(){
+//     return <h1>Hello, it VLD WOWAXUR</h1>;
+// }
+// const Welcome = () => {
+//     return <h1>Hello, it VLD WOWAXUR</h1>
+// }
+// function App (){
+//     return(<div>
+//         <a>Home </a>
+//
+//         <a>Message</a>
+//     </div>)
+// }
